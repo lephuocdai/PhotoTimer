0a14e845dd56374611e3763b27667cc6d47586e4
lephuocdai <lephuocdaijapan@gmail.com>
2015-01-15 00:12:57 +0900

    Initial commit

---
+++ PhotoTimer/PhotoTimer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoTimer
+//
+//  Created by レー フックダイ on 1/15/15.
+//  Copyright (c) 2015 lephuocdai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PhotoTimer/PhotoTimer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PhotoTimer
+//
+//  Created by レー フックダイ on 1/15/15.
+//  Copyright (c) 2015 lephuocdai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
