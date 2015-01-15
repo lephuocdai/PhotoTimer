dc484a72e629bca5c8fcb5497b826af5632a1621
lephuocdai <lephuocdaijapan@gmail.com>
2015-01-15 17:44:26 +0900

    Get url of all photos in Camera Roll

--- PhotoTimer/PhotoTimer/ViewController.h
+++ PhotoTimer/PhotoTimer/ViewController.h
@@ -7,8 +7,15 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AssetsLibrary/AssetsLibrary.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    ALAssetsLibrary *library;
+    NSArray *imageArray;
+    NSMutableArray *mutableArray;
+}
+
+-(void)allPhotosCollected:(NSArray*)imgArray;
 
 
 @end
--- PhotoTimer/PhotoTimer/ViewController.m
+++ PhotoTimer/PhotoTimer/ViewController.m
@@ -8,6 +8,9 @@
 
 #import "ViewController.h"
 
+
+static NSInteger count=0;
+
 @interface ViewController ()
             
 
@@ -18,11 +21,69 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+        [self getAllPictures];
+    });
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)getAllPictures {
+    imageArray=[[NSArray alloc] init];
+    mutableArray =[[NSMutableArray alloc]init];
+    NSMutableArray* assetURLDictionaries = [[NSMutableArray alloc] init];
+    
+    library = [[ALAssetsLibrary alloc] init];
+    
+    void (^assetEnumerator)( ALAsset *, NSUInteger, BOOL *) = ^(ALAsset *result, NSUInteger index, BOOL *stop) {
+        if(result != nil) {
+            if([[result valueForProperty:ALAssetPropertyType] isEqualToString:ALAssetTypePhoto]) {
+                [assetURLDictionaries addObject:[result valueForProperty:ALAssetPropertyURLs]];
+                
+                NSURL *url= (NSURL*) [[result defaultRepresentation]url];
+                
+                [library assetForURL:url
+                         resultBlock:^(ALAsset *asset) {
+                            
+//                             [mutableArray addObject:[UIImage imageWithCGImage:[[asset defaultRepresentation] fullScreenImage]]];
+                             [mutableArray addObject:url];
+                             NSLog(@"count = %d - url=%@",count, url);
+                             if ([mutableArray count]==count)
+                             {
+                                 imageArray=[[NSArray alloc] initWithArray:mutableArray];
+                                 [self allPhotosCollected:imageArray];
+                             }
+                             
+                         }
+                        failureBlock:^(NSError *error){ NSLog(@"operation was not successfull!"); } ];
+                
+            }
+        }
+    };
+    
+    NSMutableArray *assetGroups = [[NSMutableArray alloc] init];
+    
+    void (^ assetGroupEnumerator) ( ALAssetsGroup *, BOOL *)= ^(ALAssetsGroup *group, BOOL *stop) {
+        if(group != nil && [[group valueForProperty:@"ALAssetsGroupPropertyType"] intValue] == ALAssetsGroupSavedPhotos) {
+            [group enumerateAssetsUsingBlock:assetEnumerator];
+            [assetGroups addObject:group];
+            count=[group numberOfAssets];
+        }
+    };
+    
+    assetGroups = [[NSMutableArray alloc] init];
+    
+    [library enumerateGroupsWithTypes:ALAssetsGroupAll
+                           usingBlock:assetGroupEnumerator
+                         failureBlock:^(NSError *error) {NSLog(@"There is an error");}];
+}
+
+-(void)allPhotosCollected:(NSArray*)imgArray {
+    //write your code here after getting all the photos from library...
+    NSLog(@"all pictures are %@",imgArray);
 }
 
 @end
